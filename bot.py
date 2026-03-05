8233881269bc20f29f54e004a49ea7f879e7feea
itsyashjhapk-ux <itsyashjhapk@gmail.com>
2026-03-06 01:55:47 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,23 @@
-import os
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import Updater, CommandHandler, CallbackContext
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = "8240874907:AAGjzRy9fkZy-cvw9rN4Wmb25Noil4_M6Fk"
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Bot is running successfully!")
+def start(update: Update, context: CallbackContext):
+    update.message.reply_text("✅ Bot is running!")
+
+def help(update: Update, context: CallbackContext):
+    update.message.reply_text("Send me a file and I will generate a link.")
 
 def main():
-    app = ApplicationBuilder().token(BOT_TOKEN).build()
-    app.add_handler(CommandHandler("start", start))
-    
-    print("Bot started...")
-    app.run_polling()
+    updater = Updater(BOT_TOKEN, use_context=True)
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+
+    updater.start_polling()
+    updater.idle()
 
 if __name__ == "__main__":
     main()
