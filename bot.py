f124dbd52fdd58cf8a49a4ddbb8844ec4fb250ad
itsyashjhapk-ux <itsyashjhapk@gmail.com>
2026-03-06 01:38:24 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,36 +1,18 @@
-from pyrogram import Client, filters
-from config import BOT_TOKEN, API_ID, API_HASH, VERCEL_URL
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-app = Client(
-    "filestorebot",
-    bot_token=8240874907:AAEvesmr0JbkAAynmLLQtQBR8EOVzqMXTEM,
-    api_id=21592881,
-    api_hash=eba06a0d465f1d1797f0e92e97ac68ad
-)
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-@app.on_message(filters.command("start"))
-async def start(client, message):
-    await message.reply_text(
-        "👋 Hello!\n\nSend me any file and I will generate a secure download link."
-    )
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Bot is running successfully!")
 
-@app.on_message(filters.private & (filters.document | filters.video | filters.audio))
-async def store_file(client, message):
+def main():
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    
+    print("Bot started...")
+    app.run_polling()
 
-    if message.document:
-        file_id = message.document.file_id
-    elif message.video:
-        file_id = message.video.file_id
-    elif message.audio:
-        file_id = message.audio.file_id
-    else:
-        return
-
-    link = f"{VERCEL_URL}/access/{file_id}"
-
-    await message.reply_text(
-        f"✅ File Stored Successfully!\n\n"
-        f"🔗 Your Secure Link:\n{link}"
-    )
-
-app.run()
+if __name__ == "__main__":
+    main()
