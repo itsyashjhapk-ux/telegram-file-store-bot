0c43da561fbe5015876f6a848e281392018bb23c
itsyashjhapk-ux <itsyashjhapk@gmail.com>
2026-03-06 14:08:45 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 import os
 
 BOT_TOKEN = os.getenv("8240874907:AAGjzRy9fkZy-cvw9rN4Wmb25Noil4_M6Fk")
-API_ID = int(os.getenv("21592881"))
+API_ID = int(os.getenv("21592881")) 
 API_HASH = os.getenv("eba06a0d465f1d1797f0e92e97ac68ad")
 VERCEL_URL = os.getenv("https://heaven-verse.vercel.app")
 
@@ -13,16 +13,29 @@ app = Client(
     api_hash=API_HASH
 )
 
+# Start command
 @app.on_message(filters.command("start"))
 async def start(client, message):
-    await message.reply_text("✅ Bot is running!")
+    await message.reply_text("✅ Bot is running!\nSend me a file to generate a link.")
 
-@app.on_message(filters.document | filters.video | filters.photo)
+# File handler
+@app.on_message(filters.video | filters.document | filters.photo)
 async def file_handler(client, message):
-    file_id = message.document.file_id if message.document else message.video.file_id if message.video else message.photo.file_id
-    
+
+    if message.video:
+        file_id = message.video.file_id
+
+    elif message.document:
+        file_id = message.document.file_id
+
+    elif message.photo:
+        file_id = message.photo[-1].file_id
+
+    else:
+        return
+
     link = f"{VERCEL_URL}/access/{file_id}"
-    
-    await message.reply_text(f"🔗 Your link:\n{link}")
+
+    await message.reply_text(f"🔗 Your File Link:\n{link}")
 
 app.run()
