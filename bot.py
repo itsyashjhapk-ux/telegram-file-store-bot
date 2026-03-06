43af326ef707f96cd10f3218e01d366c2a4fa86b
itsyashjhapk-ux <itsyashjhapk@gmail.com>
2026-03-06 14:41:41 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import os
 BOT_TOKEN=os.getenv("8240874907:AAGjzRy9fkZy-cvw9rN4Wmb25Noil4_M6Fk")
 API_ID=int(os.getenv("21592881")) 
 API_HASH=os.getenv("eba06a0d465f1d1797f0e92e97ac68ad")
-VERCEL_URL= os.getenv("https://heaven-verse.vercel.app")
+VERCEL_URL=os.getenv("https://heaven-verse.vercel.app")
 
 app = Client(
     "file-store-bot",
